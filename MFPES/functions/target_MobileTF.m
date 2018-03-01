c47db490d84371ee087250e5b08967c9a685a8dd
SherryFX <feixuehuang@yahoo.com>
2018-03-02 01:05:18 +0800

    Preliminarily implement auxiliary CNN training and transfer learning

--- MFPES/functions/target_MobileTF.m
+++ MFPES/functions/target_MobileTF.m
@@ -1,4 +1,6 @@
-function [ output_args ] = target_MobileTF( input_args )
+% We evaluate the target function.
+
+function [ ret ] = target_MobileTF( input_args )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
