ed87a71af7ed8a5f0804cb977c69cda949da20c2
SherryFX <feixuehuang@yahoo.com>
2018-03-05 11:50:14 +0800

    Auxiliary and target functions

--- MFPES/functions/auxiliary_MobileTF.m
+++ MFPES/functions/auxiliary_MobileTF.m
@@ -8,7 +8,7 @@ function [ ret ] = auxiliary_MobileTF(xx, noise)
     data = imageDatastore(datasetPath, ...
         'IncludeSubfolders',true,'LabelSource','foldernames');
     
-    [trainDigitData,testDigitData] = splitEachLabel(data,0.5,'randomize');
+    [trainDigitData,testDigitData] = splitEachLabel(data,0.9,'randomize');
     
     layersTransfer = convnet.Layers(1:end-3);
     numClasses = 10;
@@ -19,14 +19,21 @@ function [ ret ] = auxiliary_MobileTF(xx, noise)
 
     % Replace parameters with xx values.
     optionsTransfer = trainingOptions('sgdm', ...
-                            'MaxEpochs',5, ...
+                            'MaxEpochs',100, ...
                             'InitialLearnRate',0.0001, ...
                             'MiniBatchSize',128);
-    
+    % Momentum 0.9 [0, 1]
+    % L2Regularization, 0.0001 (weight decay)
+                        
     t = cputime;
     netTransfer = trainNetwork(trainDigitData,layers,optionsTransfer);
-    e = cputime-t;
-    
+    e = cputime - t;
+%   (100, 0.001 , 512) 2.0740e+04, 1.9182e+04
+%   (50 , 0.001 , 512) 1.0215e+04, 9.3614e+03
+%   (25 , 0.001 , 512) 4.8956e+03, 4.9600e+03
+%   (25 , 0.0001, 512) 5.0860e+03, 4.9686e+03
+%   (100, 0.0001, 128) 1.9749e+04, 
+
     YPred = classify(netTransfer,testDigitData);
     YTest = testDigitData.Labels;
 
--- MFPES/functions/target_MobileTF.m
+++ MFPES/functions/target_MobileTF.m
@@ -2,7 +2,17 @@
 
 function [ ret ] = target_MobileTF(xx, noise)
 
+cd /Users/HFX/adb-fastboot/platform-tools;
+cmd_devices = './adb devices';
 
+cd /Users/HFX/Desktop/Bayesian\ Optimization\ on\ Smart\ Devices/MyApplication;
+cmd_build= './gradlew assembleDebug';
+cmd_install= './gradlew installDebug';
+cmd_run='/Users/HFX/adb-fastboot/platform-tools/adb shell am start -n com.example.myapplication/com.example.myapplication.MainActivity';
+
+[status,cmdout] = system(command);
+
+% process cmdout
 
 end
 
